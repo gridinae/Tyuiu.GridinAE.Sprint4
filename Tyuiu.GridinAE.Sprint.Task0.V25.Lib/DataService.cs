d7fbcd35be89e32ec8fa280fcdce75f3f903eb38
Артём <gridin350@gmail.com>
2024-11-24 13:08:56 +0500

    Add project files.

---
+++ Tyuiu.GridinAE.Sprint.Task0.V25.Lib/DataService.cs
@@ -0,0 +1,8 @@
+﻿namespace Tyuiu.GridinAE.Sprint.Task0.V25.Lib;
+
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+public class DataService : ISprint4Task0V25
+{
+    public int GetSumEvenArrEl(int[] array) => array.Where(x => x % 2 == 0).Sum();
+}
---
+++ Tyuiu.GridinAE.Sprint.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.GridinAE.Sprint.Task0.V25.Test;
+
+using Tyuiu.GridinAE.Sprint.Task0.V25.Lib;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void TestGetSumEvenArrEl()
+    {
+        DataService dataService = new();
+
+        Assert.AreEqual(18, dataService.GetSumEvenArrEl([2, 4, 3, 2, 2, 8, 9]));
+    }
+}
